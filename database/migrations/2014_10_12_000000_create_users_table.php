7f5d152aac1e9a31c4cca57d5ba37b8add9a3a28
yiyaoli <yiyao.li.2016@sis.smu.edu.sg>
2018-07-16 19:14:59 +0800

    populate db using seeder

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('user_type');
+            $table->boolean('admin') -> default(false);
             $table->rememberToken();
             $table->timestamps();
         });
