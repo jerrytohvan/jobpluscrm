b09b721dd9484bfef8898c87196e6d10dbd70298
jerrytohvan <jerrytohvan@gmail.com>
2018-07-21 01:48:56 +0800

    - Event UI forms added.

--- resources/views/layouts/index_event.blade.php
+++ resources/views/layouts/index_event.blade.php
@@ -38,54 +38,87 @@
            <div class="row">
              <div class="col-md-12">
 
-               <!-- form date pickers -->
-               <div class="x_panel" style="">
-                 <div class="x_title">
-                   <h2>Create New Event<small> Schedule your upcoming events!</small></h2>
-                   <ul class="nav navbar-right panel_toolbox">
-                     <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                     </li>
-                     <li class="dropdown">
-                       <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                       <ul class="dropdown-menu" role="menu">
-                         <li><a href="#">Settings 1</a>
-                         </li>
-                         <li><a href="#">Settings 2</a>
-                         </li>
-                       </ul>
-                     </li>
-                     <li><a class="close-link"><i class="fa fa-close"></i></a>
-                     </li>
-                   </ul>
-                   <div class="clearfix"></div>
-                 </div>
-                 <div class="x_content">
-
-
-                   <div class="well" style="overflow: auto">
-
-                     <div class="col-md-5">
-                       Date and Time
-                       <form class="form-horizontal">
-                         <fieldset>
-                           <div class="control-group">
-                             <div class="controls">
-                               <div class="input-prepend input-group" >
-                                 <span class="add-on input-group-addon"><i class="glyphicon glyphicon-calendar fa fa-calendar"></i></span>
-                                 <input type="text" name="reservation-time" id="reservation-time" class="form-control" value="01/01/2016 - 01/25/2016" />
-                               </div>
-                             </div>
-                           </div>
-                         </fieldset>
-                       </form>
-                     </div>
+               <div class="col-md-12 col-sm-12 col-xs-12">
+                 <div class="x_panel">
+                   <div class="x_title">
+                     <h2>Add New Event <small>Fill Event Details</small></h2>
+                     <ul class="nav navbar-right panel_toolbox">
+                       <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                       </li>
+                       <li class="dropdown">
+                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                         <ul class="dropdown-menu" role="menu">
+                           <li><a href="#">Settings 1</a>
+                           </li>
+                           <li><a href="#">Settings 2</a>
+                           </li>
+                         </ul>
+                       </li>
+                       <li><a class="close-link"><i class="fa fa-close"></i></a>
+                       </li>
+                     </ul>
+                     <div class="clearfix"></div>
                    </div>
+                   <div class="x_content">
+                     <br />
+                     <form id="demo-form2" data-parsley-validate class="form-horizontal form-label-left">
 
+                       <div class="form-group">
+                         <label class="control-label col-md-3 col-sm-3 col-xs-12" for="event-name">Event Name <span class="required">*</span>
+                         </label>
+                         <div class="col-md-6 col-sm-6 col-xs-12">
+                           <input type="text" id="event-name" required="required" class="form-control col-md-7 col-xs-12">
+                         </div>
+                       </div>
 
+                       <div class="form-group">
+                         <label for="middle-name" class="control-label col-md-3 col-sm-3 col-xs-12">Description</label>
+                         <div class="col-md-6 col-sm-6 col-xs-12">
+                           <input id="middle-name" class="form-control col-md-7 col-xs-12" type="text" name="middle-name">
+                         </div>
+                       </div>
+                       <div class="form-group">
+                         <label class="control-label col-md-3 col-sm-3 col-xs-12">Whatsapp Reminder? </label>
+                         <div class="col-md-6 col-sm-6 col-xs-12">
+                           <div id="wa_reminder" class="btn-group" data-toggle="buttons">
+                             <label class="btn btn-default" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
+                               <input type="radio" name="wa_reminder" value="yes"> &nbsp; Yes &nbsp;
+                             </label>
+                             <label class="btn btn-primary" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
+                               <input type="radio" name="wa_reminder" value="no"> No
+                             </label>
+                           </div>
+                         </div>
+                       </div>
+          
+
+                       <div class="form-group">
+                         <label class="control-label col-md-3 col-sm-3 col-xs-12">Date and Time <span class="required">*</span>
+                         </label>
+                           <div class="col-md-6 col-sm-6 col-xs-12">
+                               <div class="control-group">
+                                 <div class="controls">
+                                   <div class="input-prepend input-group" >
+                                     <span class="add-on input-group-addon"><i class="glyphicon glyphicon-calendar fa fa-calendar"></i></span>
+                                     <input type="text" name="reservation-time" id="reservation-time" class="form-control" value="01/01/2016 - 01/25/2016" />
+                                   </div>
+                                 </div>
+                               </div>
+                            </div>
+                        </div>
+                       <div class="ln_solid"></div>
+                       <div class="form-group">
+                         <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+                           <button class="btn btn-primary" type="button">Cancel</button>
+               <button class="btn btn-primary" type="reset">Reset</button>
+                           <button type="submit" class="btn btn-success">Submit</button>
+                         </div>
+                       </div>
+
+                     </form>
+                   </div>
                  </div>
                </div>
-               <!-- /form datepicker -->
-
              </div>
            </div>
            <div class="row">
