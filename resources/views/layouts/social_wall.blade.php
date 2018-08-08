3cb541248d956a0fc1a37f6614b694b683fb17b7
njiajia <jiajia.ng.2016@sis.smu.edu.sg>
2018-08-08 21:07:37 +0800

    resize profile picture

--- resources/views/layouts/social_wall.blade.php
+++ resources/views/layouts/social_wall.blade.php
@@ -11,11 +11,10 @@
   </div>
 
   <section class="row new-post">
-    
     <!--Add profile img-->
     <div class="col-md-2 col-md-offset-1">
         <div class="x_panel">
-            <img class="center-block" src="https://secure.gravatar.com/avatar/ef76f4019c437fa1ea0b8345486ba571?s=80&r=g&d=identicon">
+            <img class="center-block" src="https://secure.gravatar.com/avatar/ef76f4019c437fa1ea0b8345486ba571?s=80&r=g&d=identicon" style="width:129px; height:129px;"/>
         </div>
     </div>
     <!-- /Add profile img -->
@@ -43,17 +42,23 @@
                   <div class="info">
                       Posted by {{ $post->user->name }} on {{ $post->created_at }}
                   </div>
-                  <div class="interaction">
-                    <!-- Should use icon when liked, light up icon and grey icon -->
-                      <a href="#" class="like">{{ Auth::user()->likes()->where('post_id', $post->id)->first() ? Auth::user()->likes()->where('post_id', $post->id)->first()->like == 1 ? 'You like this post' : 'Like' : 'Like'  }}</a> |
-                      @if(Auth::user() == $post->user)
 
-                          <a data-id="{{ $post->id }}" data-content="{{ $post->content }}" class="edit" id="Edit-modal"
-                                 href="#edit-modal">Edit</a>
-                          |
-                          <a href="{{ route('delete.post', ['post_id' => $post->id]) }}">Delete</a>
+                  <!-- Adding button  -->
+                  <div class="interaction">
+                    
+                    <!-- Should use icon when liked, light up icon and grey icon -->
+                      <button type="button" class="btn btn-default btn-xs fa fa-heart-o"><a href="#" class="like">{{ Auth::user()->likes()->where('post_id', $post->id)->first() ? Auth::user()->likes()->where('post_id', $post->id)->first()->like == 1 ? 'You like this post' : 'Like' : 'Like'  }}</a></button> 
+                      @if(Auth::user() == $post->user) 
+
+                      <button type="button" class="btn btn-default btn-xs fa fa-edit"><a data-id="{{ $post->id }}" data-content="{{ $post->content }}" class="edit" id="Edit-modal"
+                                 href="#edit-modal">Edit</a></button>
+                          
+                      <button type="button" class="btn btn-default btn-xs fa fa-trash"><a href="{{ route('delete.post', ['post_id' => $post->id]) }}">Delete</a></button>
+                        
                       @endif
                   </div>
+                  <!-- /Adding button  -->
+                  
               </article>
             </div>  <!-- /panel for post -->
           @endforeach
