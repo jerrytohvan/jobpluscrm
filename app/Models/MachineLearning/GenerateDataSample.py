2d92cd635cb244f76293d77969ffe890f012fdcd
jerrytohvan <jerrytohvan@gmail.com>
2018-08-12 23:57:40 +0800

    `GenerateDataSample.py` v1 - ML Script combines job posting from 6 different data source (141,252 data collected) as a base of Apriori association algorithm

--- app/Models/MachineLearning/GenerateDataSample.py
+++ app/Models/MachineLearning/GenerateDataSample.py
@@ -2,7 +2,7 @@ import csv,re
 
 def retrieve_years_of_experience(desc):
     word = -1
-    years_array = [desc.rfind('years of'),desc.rfind('years of experience'), desc.rfind('years of work'),desc.rfind('years of professional'),desc.rfind('years of total experience')]
+    years_array = [desc.rfind('years of'),desc.rfind('years of experience'),desc.rfind('year of'), desc.rfind('years of work'),desc.rfind('years of professional'),desc.rfind('years of total experience')]
     for x in years_array:
         if x != -1:
             x = desc[x-10:x+10]
@@ -27,38 +27,44 @@ with open('./Data/dice_com-job_us_sample.csv', 'rb') as f:
     next(f)
     reader = csv.reader(f, delimiter=',')
     for row in reader:
-        # word = -1
-        # years_array = [row[3].rfind('years of experience'), row[3].rfind('years of work'),row[3].rfind('years of professional'),row[3].rfind('years of total experience')]
-        # years_experience_index = max(years_array)
-        # if years_experience_index != -1:
-        #     substring = row[3][years_experience_index-10:years_experience_index+10]
-        #     word = -1 if not substring else (re.findall(r'\d+', substring))
-        #     if word != -1 and len(word) > 1:
-        #         word = word[-1]
-        # if isinstance(word, list):
-        #     word = "".join(map(str, word))
         word = retrieve_years_of_experience(row[3])
         container.append([row[6], row[3], '', row[10], '', word])
 
 #Sample Data from GOOGLE
-# with open('./Data/job_skills.csv', 'rb') as f:
-#     next(f)
-#     reader = csv.reader(f, delimiter=',')
-#     for row in reader:
-#         word = retrieve_years_of_experience(row[5])
-#         print(word)
-#         container.append([row[1], row[4], row[2], row[6], '', word])
-
-#Sample Data from naukri_com-job-sample
-with open('./Data/naukri_com-job-sample.csv', 'rb') as f:
+with open('./Data/job_skills.csv', 'rb') as f:
     next(f)
     reader = csv.reader(f, delimiter=',')
     for row in reader:
-        word = row[2][row[2].rfind('yrs')-10:row[2].rfind('yrs')+10]
-        print(word)
-        container.append([row[7], row[5], '', row[1] + row[12], row[4], word])
+        word = retrieve_years_of_experience(row[5])
+        container.append([row[1], row[4], row[2], row[6], '', word])
+
+#Sample Data from naukri_com-job-sample
+with open('./Data/naukri_com-job_sample.csv', 'rb') as f:
+    next(f)
+    reader = csv.reader(f, delimiter=',')
+    for row in reader:
+        word = -1 if not row[2] else (re.findall(r'\d+', row[2]))
+        if word != -1 and len(word) > 1:
+            word = word[0]
+        container.append([row[7], row[4], '', row[1] + row[12], row[4], word])
+
+#Sample Data from reed_uk
+with open('./Data/reed_uk.csv', 'rb') as f:
+    next(f)
+    reader = csv.reader(f, delimiter=',')
+    for row in reader:
+        word = retrieve_years_of_experience(row[5])
+        cat =  row[0][0:row[0].rfind(' ')]
+        container.append([row[7], row[5], cat ,row[6] + row[5] , cat, word])
 
 
+#Sample Data from data_job_posts
+with open('./Data/data_job_posts.csv', 'rb') as f:
+    next(f)
+    reader = csv.reader(f, delimiter=',')
+    for row in reader:
+        word = retrieve_years_of_experience(row[13])
+        container.append([row[2], row[11], '' ,row[12] + row[13] , '', word])
 
 #write to csv
 with open("./Data/data_samples.csv","wb") as doc:
