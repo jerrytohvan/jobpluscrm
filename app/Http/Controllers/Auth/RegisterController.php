bdd76f46c535b39519512e718b07170553a42bdf
jerrytohvan <jerrytohvan@gmail.com>
2018-08-19 23:26:06 +0800

    clean up firebase & RegisterController edited

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -11,73 +11,72 @@ use Illuminate\Foundation\Auth\RegistersUsers;
 
 class RegisterController extends Controller
 {
-	/*
-	|--------------------------------------------------------------------------
-	| Register Controller
-	|--------------------------------------------------------------------------
-	|
-	| This controller handles the registration of new users as well as their
-	| validation and creation. By default this controller uses a trait to
-	| provide this functionality without requiring any additional code.
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Register Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users as well as their
+    | validation and creation. By default this controller uses a trait to
+    | provide this functionality without requiring any additional code.
+    |
+    */
 
-	// use RegistersUsers;
+    // use RegistersUsers;
 
-	/**
-	 * Where to redirect users after login / registration.
-	 *
-	 * @var string
-	 */
-	// protected $redirectTo = '/register';
-	// protected $registerView = 'layouts.register';
+    /**
+     * Where to redirect users after login / registration.
+     *
+     * @var string
+     */
+    // protected $redirectTo = '/register';
+    // protected $registerView = 'layouts.register';
 
-	/**
-	 * Create a new controller instance.
-	 *
-	 * @return void
-	 */
-	public function __construct()
-	{
-		$this->middleware('auth');
-	}
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('web');
+    }
 
-	public function index()
-	{
-		return Auth::user()->admin == 1 ? view('layouts.register'): abort(403, 'Unauthorized action.');
-	}
+    public function index()
+    {
+        return Auth::user()->admin == 1 ? view('layouts.register'): abort(403, 'Unauthorized action.');
+    }
 
-	/**
-	 * Get a validator for an incoming registration request.
-	 *
-	 * @param  array  $data
-	 * @return \Illuminate\Contracts\Validation\Validator
-	 */
-	protected function validator(array $data)
-	{
-		return Validator::make($data, [
-			'name' => 'required|max:255',
-			'email' => 'required|email|max:255|unique:users',
-			'password' => 'required|min:6|confirmed',
-			//user type verification
-		]);
-	}
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|min:6|confirmed',
+            //user type verification
+        ]);
+    }
 
-	/**
-	 * Create a new user instance after a valid registration.
-	 *
-	 * @param  array  $data
-	 * @return User
-	 */
-	protected function register(Request $data)
-	{
-		$user = User::create([
-			'name' => $data['name'],
-			'email' => $data['email'],
-			'password' => bcrypt($data['password']),
-			'admin' => $data['admin']
-		]);
-		return view('layouts.register');
-
-	}
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return User
+     */
+    protected function register(Request $data)
+    {
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+            'admin' => $data['admin']
+        ]);
+        return view('layouts.register');
+    }
 }
