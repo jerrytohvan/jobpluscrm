a916a56c238ab2808dcdaaac3ed835fbb88d8a74
jerrytohvan <jerrytohvan@gmail.com>
2018-08-19 21:14:25 +0800

    resolve `Company.php` conflict

--- app/Models/Clients/Company.php
+++ app/Models/Clients/Company.php
@@ -6,18 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
-<<<<<<< HEAD
-  protected $guarded = [];
-=======
-  protected $guarded =  [];
->>>>>>> 221d2b60... api cleaned up and edited candidates model
+    protected $guarded =  [];
 
-  public function employees()
-  {
-      return $this->hasMany('App\Models\Employees\Employee');
-  }
-  public function events()
-  {
-      return $this->belongsToMany('App\Models\Events\Event');
-  }
+    public function employees()
+    {
+        return $this->hasMany('App\Models\Employees\Employee');
+    }
+    public function events()
+    {
+        return $this->belongsToMany('App\Models\Events\Event');
+    }
 }
