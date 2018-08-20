37449e9e633e5d566d3cca213d778376fbff2ea9
jerrytohvan <jerrytohvan@gmail.com>
2018-08-21 01:00:12 +0800

    BUG: storage folder cant be access (temp: public folder)

--- app/Models/MachineLearning/SmartMatchController.php
+++ app/Models/MachineLearning/SmartMatchController.php
@@ -21,14 +21,14 @@ class SmartMatchController extends Controller
     public function matchDescriptionWithPotentialJobs()
     {
         $file = request()->file('resume');
-        $path = public_path()."/storage/";
+        $path = public_path()."/";
         $file->move($path, $file->getClientOriginalName());
         $original_name = $file->getClientOriginalName();
         $interest = explode(',', request()->get('interest'));
         $jobDesc = $this->svc->extract_keywords(request()->get('job_description'));
         $skills =  $this->svc->extract_keywords(request()->get('skills'));
         $additionalQuery = array_merge($interest, array_merge($skills, $jobDesc));
-        unlink(realpath($_SERVER["DOCUMENT_ROOT"])."/storage/".$original_name);
+        unlink(realpath($_SERVER["DOCUMENT_ROOT"])."/". $original_name);
         $results = $this->svc->matchResumeWithSampleData($original_name, $additionalQuery, 1);
         return view('layouts.results_smart_match', compact('results'));
     }
