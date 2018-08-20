03e780679314a4be46b61b2872c9623b861bff74
jerrytohvan <jerrytohvan@gmail.com>
2018-08-20 16:51:16 +0800

    DB_DATABASE env variable added

--- config/database.php
+++ config/database.php
@@ -63,7 +63,7 @@ return [
             'driver' => 'pgsql',
             'host' => env('DB_HOST'),
             'port' =>   env('DB_PORT'),
-            'database' =>   env('DB_HOST'),
+            'database' =>   env('DB_DATABASE'),
             'username' =>  env('DB_USERNAME'),
             'password' =>  env('DB_PASSWORD'),
             'charset' => 'utf8',
