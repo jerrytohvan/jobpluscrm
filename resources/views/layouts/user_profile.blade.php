e66bb6ee55e6925c1592b74e6cdf83225a798bf6
WaiLeng <waileng.woo.2016@sis.smu.edu.sg>
2018-08-29 18:17:36 +0800

    Update the edit profile form (modal) with the following fields: - name, email, mobile, birth date and display picture.

--- resources/views/layouts/user_profile.blade.php
+++ resources/views/layouts/user_profile.blade.php
@@ -41,9 +41,8 @@
                         <div class="profile_img">
                           <div id="crop-avatar">
                             <!-- Current avatar -->
-                            <!-- <img class="img-responsive avatar-view" src="images/picture.jpg" alt="Avatar" title="Change the avatar"> -->
-                            <img class="img-responsive avatar-view" src="{{ ($user->profile_pic != null ? $user->profile_pic : Gravatar::src(Auth::user()->email)) }}" alt="Avatar" title="Change the avatar">
-
+                            <!-- <img class="img-responsive avatar-view" src="{{ ($user->profile_pic != null ? $user->profile_pic : Gravatar::src(Auth::user()->email)) }}" alt="Avatar" title="Change the avatar"> -->
+                            <img class="img-responsive avatar-view" src="{{ ($user->profilepic) }}" alt="Avatar" title="Change the avatar">
                           </div>
                         </div>
                         <h3>{{ $user->name }}</h3>
@@ -226,32 +225,44 @@
                             <h4 class="modal-title">Edit Profile</h4>
                         </div>
                         <div class="modal-body">
-                          {{  Form::open(['method'=>'post', 'data-parsley-validate', 'class' => 'form-horizontal form-label-left', 'id'=>'modal_form_id', 'enctype'=>'multipart/form-data']) }}
+                          {{  Form::open(['route' => 'edit.profile','method'=>'post', 'data-parsley-validate', 'class' => 'form-horizontal form-label-left', 'id'=>'modal_form_id', 'enctype'=>'multipart/form-data']) }}
+
+                                <div class="form-group">
+                                    <label for="name">Name</label>
+                                    <input id="name" class="form-control col-md-7 col-xs-12" required="required" type="text" name="name" value="{{ $user->name }}">
+                                </div>
 
                                 <div class="form-group">
                                     <label for="email">Email</label>
                                     <input id="email" class="form-control col-md-7 col-xs-12" required="required" type="email" name="email" value="{{ $user->email }}">
                                 </div>
-                                <br/><br/>
 
-                                    <div class="form-group">
-                                        <label for="birthday">Birth Date</label>
-
-                                        <input type="text" class="form-control col-md-7 col-xs-12"  data-date-format="MM/DD/YYYY" required="required" id="birthday" name="birthday" value='{{ date("mm/dd/YYYY", strtotime($user->birth_date)) }}' />
-
-                                      </div>
-                                      <br/>
-                                      <br/>
+                                <!-- <div class="form-group">
+                                    <label for="password">Password</label>
+                                    <input id="password" class="form-control col-md-7 col-xs-12" required="required" type="password" name="password" value="{{ $user->password }}">
+                                </div> -->
 
                                 <div class="form-group">
-                                    <label for="photo">Display Picture</label>
-                                    <input type="file" name="photo" id="validate_img" required="required" data-parsley-filemaxmegabytes="1" data-parsley-trigger="change" data-parsley-filemimetypes="image/jpeg, image/pipeg, image/png, image/bmp, image/webp">
-                                </div>
+                                    <label for="handphone">Mobile</label>
+                                    <input id="handphone" class="form-control col-md-7 col-xs-12" required="required" type="text" name="handphone" value="{{ $user->handphone }}">
+                                </div>    
+
+                                <div class="form-group">
+                                     <label for="birthday">Birth Date</label>
+                                     <input type="text" class="form-control col-md-7 col-xs-12"  data-date-format="MM/DD/YYYY" required="required" id="birthday" name="birthday" value='{{ date("mm/dd/YYYY", strtotime($user->birth_date)) }}' />
+                                </div> 
+
+                                <div class="form-group">
+                                  <label for="photo">Display Picture</label>
+                                  <input type="file" name="photo" id="photo" required="required" data-parsley-filemaxmegabytes="1" data-parsley-trigger="change" data-parsley-filemimetypes="image/jpeg, image/pipeg, image/png, image/bmp, image/webp">
+                               </div> 
+
                             </form>
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
                             <button type="button" class="btn btn-primary"  onclick="submitform();" >Update Profile</button>
+                            {!! Form::close() !!}
                         </div>
                     </div><!-- /.modal-content -->
 
@@ -286,7 +297,10 @@ function submitform()
     var urlEdit = '{{ route('edit.profile') }}';
     var postData = new FormData($("#modal_form_id")[0]);
     var postId = 0;
-    console.log(postData);
+
+    var name = $("input[name=name]").val();
+    var email = $("input[name=email]").val();
+ 
     $(document).ready(function () {
       $(".edit").click(function () {
           id = $(this).data('id');
@@ -298,9 +312,10 @@ function submitform()
           var saveData = $.ajax({
           type: 'POST',
           url: urlEdit,
-          processData: false,
-          contentType: false,
+          // processData: false,
+          // contentType: false,
           data : postData,
+          // data:{name:name,email:email},
           success: function(resultData) {
               window.location.reload();
                }
@@ -316,8 +331,6 @@ function submitform()
           maxYear: parseInt(moment().format('YYYY'),10)
         })
       });
-
-
 
       var app = app || {};
 
