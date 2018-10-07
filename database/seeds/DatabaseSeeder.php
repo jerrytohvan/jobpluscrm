f4ca6a07b51dc5b31574223df1b104284bcf535e
jerrytohvan <jerrytohvan@gmail.com>
2018-10-08 00:31:59 +0800

    Temp user table seeders for migration update

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,15 +17,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
-        $this->call(CompaniesTableSeeder::class);
+        // $this->call(CompaniesTableSeeder::class);
         //$this->call(EmployeesTableSeeder::class);
         //$this->call(ProjectGroupsTableSeeder::class);
         //$this->call(CandidatessTableSeeder::class);
-        $this->call(ResultsTableSeeder::class);
-        $this->call(InterestsTableSeeder::class);
-        $this->call(FieldsTableSeeder::class);
-        $this->call(MessagesTableSeeder::class);
-        $this->call(LikesTableSeeder::class);
+        // $this->call(ResultsTableSeeder::class);
+        // $this->call(InterestsTableSeeder::class);
+        // $this->call(FieldsTableSeeder::class);
+        // $this->call(MessagesTableSeeder::class);
+        // $this->call(LikesTableSeeder::class);
         // $this->svc->setDataIntoDB(public_path()  . '/data_samples.csv');
     }
 }
