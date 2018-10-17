705cc830664c25fc7b7fb9a4df427e0a2d53b116
yiyaoli <yiyao.li.2016@sis.smu.edu.sg>
2018-10-17 20:45:21 +0800

    sorted the company name by asc

--- app/Models/Jobs/JobController.php
+++ app/Models/Jobs/JobController.php
@@ -36,7 +36,7 @@ class JobController extends Controller
     {
         $message = "";
         $status = "";
-        $companies = Company::all();
+        $companies = Company::all()->sortBy('name');
         return view('layouts.job_new', compact('status', 'message', 'companies'));
     }
 }
