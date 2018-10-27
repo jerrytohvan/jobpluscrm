984219dd1f44fd9a135702e3387022ef57cd27a8
yiyaoli <yiyao.li.2016@sis.smu.edu.sg>
2018-10-27 13:18:45 +0800

    uncommented dummy.index

--- routes/web.php
+++ routes/web.php
@@ -300,9 +300,9 @@ Route::group(['middleware' => ['auth']], function () {
 
 
 
-    // Route::get('/tasks/dummyTask',[
-    //   'as' => 'dummy.index',
-    //   'uses' => '\App\Models\Tasks\TaskController@topfew']);
+    Route::get('/tasks/dummyTask',[
+      'as' => 'dummy.index',
+      'uses' => '\App\Models\Tasks\TaskController@topfew']);
     // Route::get('/tasks/data', '\App\Models\Tasks\TaskController@display');
     Route::get('/tasks/show', '\App\Models\Tasks\TaskController@showTaskList');
 
