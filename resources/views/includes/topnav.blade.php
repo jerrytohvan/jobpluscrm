4d988822b8c5b37ceea0e76356e9a453b3506f35
Gabriel Ong <gabriel.ong.2016@sis.smu.edu.sg>
2018-11-01 22:04:02 +0800

    topnav hidden modal

--- resources/views/includes/topnav.blade.php
+++ resources/views/includes/topnav.blade.php
@@ -85,3 +85,154 @@
     </nav>
   </div>
 </div>
+
+
+
+
+
+
+
+
+
+
+<!-- hidden model-->
+
+
+
+
+<div class="modal fade" tabindex="-1" role="dialog" id="new-email">
+<div class="modal-dialog">
+   <div class="modal-content">
+      <div class="modal-header">
+         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+
+
+
+              <div class="compose col-md-6 col-xs-12">
+                <div class="compose-header">
+                  New Message
+                  <button type="button" class="close compose-close">
+                    <span>×</span>
+                  </button>
+                </div>
+
+                <div class="compose-body">
+                  <div id="alerts"></div>
+
+                  <div class="btn-toolbar editor" data-role="editor-toolbar" data-target="#editor">
+                    <div class="btn-group">
+                      <a class="btn dropdown-toggle" data-toggle="dropdown" title="Font"><i class="fa fa-font"></i><b class="caret"></b></a>
+                      <ul class="dropdown-menu">
+                      </ul>
+                    </div>
+
+                    <div class="btn-group">
+                      <a class="btn dropdown-toggle" data-toggle="dropdown" title="Font Size"><i class="fa fa-text-height"></i>&nbsp;<b class="caret"></b></a>
+                      <ul class="dropdown-menu">
+                        <li>
+                          <a data-edit="fontSize 5">
+                            <p style="font-size:17px">Huge</p>
+                          </a>
+                        </li>
+                        <li>
+                          <a data-edit="fontSize 3">
+                            <p style="font-size:14px">Normal</p>
+                          </a>
+                        </li>
+                        <li>
+                          <a data-edit="fontSize 1">
+                            <p style="font-size:11px">Small</p>
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+
+                    <div class="btn-group">
+                      <a class="btn" data-edit="bold" title="Bold (Ctrl/Cmd+B)"><i class="fa fa-bold"></i></a>
+                      <a class="btn" data-edit="italic" title="Italic (Ctrl/Cmd+I)"><i class="fa fa-italic"></i></a>
+                      <a class="btn" data-edit="strikethrough" title="Strikethrough"><i class="fa fa-strikethrough"></i></a>
+                      <a class="btn" data-edit="underline" title="Underline (Ctrl/Cmd+U)"><i class="fa fa-underline"></i></a>
+                    </div>
+
+                    <div class="btn-group">
+                      <a class="btn" data-edit="insertunorderedlist" title="Bullet list"><i class="fa fa-list-ul"></i></a>
+                      <a class="btn" data-edit="insertorderedlist" title="Number list"><i class="fa fa-list-ol"></i></a>
+                      <a class="btn" data-edit="outdent" title="Reduce indent (Shift+Tab)"><i class="fa fa-dedent"></i></a>
+                      <a class="btn" data-edit="indent" title="Indent (Tab)"><i class="fa fa-indent"></i></a>
+                    </div>
+
+                    <div class="btn-group">
+                      <a class="btn" data-edit="justifyleft" title="Align Left (Ctrl/Cmd+L)"><i class="fa fa-align-left"></i></a>
+                      <a class="btn" data-edit="justifycenter" title="Center (Ctrl/Cmd+E)"><i class="fa fa-align-center"></i></a>
+                      <a class="btn" data-edit="justifyright" title="Align Right (Ctrl/Cmd+R)"><i class="fa fa-align-right"></i></a>
+                      <a class="btn" data-edit="justifyfull" title="Justify (Ctrl/Cmd+J)"><i class="fa fa-align-justify"></i></a>
+                    </div>
+
+                    <div class="btn-group">
+                      <a class="btn dropdown-toggle" data-toggle="dropdown" title="Hyperlink"><i class="fa fa-link"></i></a>
+                      <div class="dropdown-menu input-append">
+                        <input class="span2" placeholder="URL" type="text" data-edit="createLink" />
+                        <button class="btn" type="button">Add</button>
+                      </div>
+                      <a class="btn" data-edit="unlink" title="Remove Hyperlink"><i class="fa fa-cut"></i></a>
+                    </div>
+                    <div class="btn-group">
+                      <a class="btn" data-edit="undo" title="Undo (Ctrl/Cmd+Z)"><i class="fa fa-undo"></i></a>
+                      <a class="btn" data-edit="redo" title="Redo (Ctrl/Cmd+Y)"><i class="fa fa-repeat"></i></a>
+                    </div>
+                  </div>
+
+
+
+
+                      {{  Form::open(['route' =>'sendemail', 'method'=>'post','id'=>'submit-email','files'=> true,'enctype'=>'multipart/form-data']) }}
+
+                      <!-- {{  Form::open(['route' =>'sendemail', 'method'=>'post','id'=>'submit-email','enctype'=>'multipart/form-data']) }} -->
+
+
+
+
+
+
+                  <!--  <div class="form-group">
+                      <a class="btn" title="Insert picture (or just drag & drop)" id="emailAttachment"><i class="fa fa-picture-o"></i></a>
+                      <input type="file" data-role="magic-overlay" data-target="#pictureBtn" data-edit="insertImage" />
+                    </div> -->
+
+
+                    </div>
+
+                    <div class="form-group">
+                      {!! Form::label('emailAttachment', 'Upload Attachment:(only 1 at a time)') !!}
+                      {!! Form::file('emailAttachment', null, ['class' => 'form-control']) !!}
+                    </div>
+
+                    <div class="form-group">
+                    <label for="toEmail">TO*: (comma seperated email addresses)</label>
+                    <input type="email" id="toEmail" multiple pattern="^([\w+-.%]+@[\w-.]+\.[A-Za-z]{2,4},*[\W]*)+$"  class="form-control parseley-error" data-parsley-trigger="change" name="toEmail" required="required" >
+                  </div>
+
+                  <div class="form-group">
+                  <label for="ccEmail">CC: (comma seperated email addresses)</label>
+                  <input type="email" id="ccEmail" multiple pattern="^([\w+-.%]+@[\w-.]+\.[A-Za-z]{2,4},*[\W]*)+$" class="form-control" name="ccEmail" data-parsley-trigger="change" >
+                  </div>
+
+
+                  <div class="form-group">
+                  <label for="subject">Subject * :</label>
+                  <input type="text" id="subject" class="form-control parsley-error" name="subject" data-parsley-trigger="change" required="required">
+                  </div>
+
+                <div class="form-group">
+                    <div id="editor"  class="editor-wrapper"></div>
+                    <input type="hidden" id="emailMessage" value="" name="emailMessage"/>
+              </div>
+                 <div class="compose-footer">
+                {{ Form::submit('Send', ['class'=>'btn btn-sm btn-success']) }}
+                </div>
+                              {!! Form::close() !!}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
