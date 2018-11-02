eedcad04d8810e863f3ac2b87bb8fac69dd2231d
Gabriel Ong <gabriel.ong.2016@sis.smu.edu.sg>
2018-11-02 13:18:25 +0800

    Sidebar updated

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -34,9 +34,10 @@
         <!-- sidebar menu -->
         <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
           <div class="menu_section">
-            <h3>General</h3>
+          <span>.</span>
             <ul class="nav side-menu">
               <li>
+                <span></span>
                 <a href="{{ route('dashboard') }}"><i class="fa fa-home"></i> Home </a>
               </li>
 
