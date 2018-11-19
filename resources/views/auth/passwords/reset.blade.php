a0d826985d3dba3a53c41d75a82e8882593f0c52
jerrytohvan <jerrytohvan@gmail.com>
2018-11-19 19:33:17 +0800

    UI Fix on Gentellela UI

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -55,9 +55,9 @@
                         <br />
 
                         <div>
-                            <h1><i class="fa fa-paw"></i> Gentelella Alela!</h1>
-                            <p>©2016 All Rights Reserved. Gentelella Alela! is a Bootstrap 3 template. Privacy and Terms</p>
-                        </div>
+              						<h1><i class="fa fa-odnoklassniki"></i> JobPlus </h1>
+              						<p>©2018 All Rights Reserved. Artwork by <a href="https://http://jobplus.sg/">JobPlus</a></p>
+              					</div>
                     </div>
                 {!! BootForm::close() !!}
             </section>
