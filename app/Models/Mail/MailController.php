7b3aa174eac92ee4835977335535b8cb4f3020ce
Gabriel Ong <gabriel.ong.2016@sis.smu.edu.sg>
2018-11-21 10:05:09 +0800

    spelling

--- app/Models/Mail/MailController.php
+++ app/Models/Mail/MailController.php
@@ -72,7 +72,7 @@ class MailController extends Controller
             $msg = str_replace("<br>","\n",$msg);
             $msg = str_replace("</div>","",$msg);
             $msg = str_replace("</li><li>","\n",$msg);
-            $msg = str_replace("&nspb"," ",$msg);
+            $msg = str_replace("&nbsp"," ",$msg);
             $msg = strip_tags($msg);
             $message->setBody($msg);
         });
