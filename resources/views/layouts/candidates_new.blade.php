9dbdda8380826713c795f591b145b4cbb2fa6059
Gabriel Ong <gabriel.ong.2016@sis.smu.edu.sg>
2018-11-25 14:41:54 +0800

    Change birthdate to use Current date instead

--- resources/views/layouts/candidates_new.blade.php
+++ resources/views/layouts/candidates_new.blade.php
@@ -93,12 +93,12 @@
                 </div>
               </div>
 
-              <div class="form-group">
+              <!-- <div class="form-group">
                   <label class="control-label col-md-3 col-sm-3 col-xs-12" for="birthdate">Birth Date *</label>
                   <div class="col-md-6 col-sm-6 col-xs-12">
                   <input type="date" class="form-control col-md-7 col-xs-12"  data-date-format="MM/DD/YYYY" required="required" id="birthdate" name="birthdate" value='' />
                 </div>
-              </div>
+              </div> -->
 
               <div class="form-group">
                   <label class="control-label col-md-3 col-sm-3 col-xs-12" for="industry">Industry *</label>
